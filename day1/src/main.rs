77e58f66d2b5531ae33bd9d02d1e0c97606bb437
Arne Dußin <arne.dussin@live.de>
2019-12-04 13:40:51 +0100

    Correct solution to calculate additional fuel on module basis

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -22,7 +22,13 @@ fn main() {
         let line = line.unwrap();
 
         let module_mass = line.parse::<u64>().expect("Module mass was not int");
-        let fuel_required = fuel_for_mass(module_mass);
+        let mut fuel_required = fuel_for_mass(module_mass);
+
+        let mut fuel_for_fuel_added = fuel_required;
+        while fuel_for_fuel_added != 0 {
+            fuel_for_fuel_added = fuel_for_mass(fuel_for_fuel_added);
+            fuel_required += fuel_for_fuel_added;
+        }
 
         total_mass += module_mass;
         total_fuel_required += fuel_required;
@@ -30,19 +36,4 @@ fn main() {
 
     println!("Total mass is: {}", total_mass);
     println!("Total fuel required is: {}", total_fuel_required);
-
-    println!("Calculating fuel for fuel.");
-
-    let mut fuel_added = total_fuel_required;
-    while fuel_added != 0 {
-        fuel_added = fuel_for_mass(fuel_added);
-        total_fuel_required += fuel_added;
-
-        println!("Adding {} fuel for the added fuel", fuel_added);
-    }
-
-    println!(
-        "Total fuel after accounting for additional fuel: {}",
-        total_fuel_required
-    );
 }
