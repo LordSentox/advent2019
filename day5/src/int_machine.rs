e311c6dc332d9fffe5e2df451cd0164bbac2ae40
Arne Dußin <arne.dussin@live.de>
2019-12-09 11:27:09 +0100

    Add still buggy int code machine v2

--- day5/src/int_machine.rs
+++ day5/src/int_machine.rs
@@ -1,56 +1,89 @@
-use std::fs;
+use num_traits::FromPrimitive;
 use std::io::{stdin, stdout, Write};
 
 // The integer type used in the inputs
-pub type Int = u32;
+pub type Int = i64;
 
-// The exit code used by the int input programs
-const CODE_EXIT: Int = 99;
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Primitive)]
+pub enum Operation {
+    Add = 1,
+    Mul = 2,
+    Inp = 3,
+    Out = 4,
+    Exit = 99,
+}
 
-const CODE_ADD: Int = 1;
-const CODE_MUL: Int = 2;
-const CODE_INP: Int = 3;
-const CODE_OUT: Int = 4;
+impl Operation {
+    pub fn num_params(self) -> usize {
+        use Operation::*;
+        match self {
+            Add => 3,
+            Mul => 3,
+            Inp => 1,
+            Out => 1,
+            Exit => 0,
+        }
+    }
+}
 
 pub enum Mode {
     Address,
-    Immediate
+    Immediate,
 }
 
-pub fn modes_from_opcode()
+fn split_op_code(op_code: Int) -> (Operation, Vec<Mode>) {
+    let op = Operation::from_u8((op_code % 100) as u8).expect("Invalid op code");
+
+    // Firstly, we will ignore the operation part of our opcode, so the least significant two
+    // digits will be cut off.
+    let mut op_code = op_code / 100;
+    let mut modes = Vec::new();
+    while op_code != 0 {
+        match op_code % 10 {
+            1 => modes.push(Mode::Immediate),
+            0 => modes.push(Mode::Address),
+            other => panic!("{} is not a valid parameter mode", other),
+        }
+
+        op_code /= 10;
+    }
+
+    (op, modes)
+}
+
+fn resolve_pos(pos: usize, modes: &Vec<Mode>, tape: &Vec<Int>) -> usize {
+    match modes.get(pos) {
+        Some(Mode::Address) | None => tape[pos] as usize,
+        Some(Mode::Immediate) => pos,
+    }
+}
 
 fn add(tape: &mut Vec<Int>, pos: usize, modes: Vec<Mode>) {
     let res_pos = tape[pos + 3] as usize;
+    let p1_pos = resolve_pos(pos + 1, &modes, &tape);
+    let p2_pos = resolve_pos(pos + 2, &modes, &tape);
 
     print!(
         "@{}[{}] + @{}[{}] => @{}[{:?} ->",
-        tape[pos + 1],
-        tape[tape[pos + 1] as usize],
-        tape[pos + 2],
-        tape[tape[pos + 2] as usize],
-        tape[pos + 3],
-        tape[res_pos]
+        p1_pos, tape[p1_pos], p2_pos, tape[p2_pos], res_pos, tape[res_pos]
     );
 
-    tape[res_pos] = tape[tape[pos + 1] as usize] + tape[tape[pos + 2] as usize];
+    tape[res_pos] = tape[p1_pos] + tape[p2_pos];
 
     println!(" {}]", tape[res_pos]);
 }
 
-fn mul(tape: &mut Vec<Int>, pos: usize) {
+fn mul(tape: &mut Vec<Int>, pos: usize, modes: Vec<Mode>) {
     let res_pos = tape[pos + 3] as usize;
+    let p1_pos = resolve_pos(pos + 1, &modes, &tape);
+    let p2_pos = resolve_pos(pos + 2, &modes, &tape);
 
     print!(
         "@{}[{}] * @{}[{}] => @{}[{:?} ->",
-        tape[pos + 1],
-        tape[tape[pos + 1] as usize],
-        tape[pos + 2],
-        tape[tape[pos + 2] as usize],
-        tape[pos + 3],
-        tape[res_pos]
+        p1_pos, tape[p1_pos], p2_pos, tape[p2_pos], res_pos, tape[res_pos]
     );
 
-    tape[res_pos] = tape[tape[pos + 1] as usize] * tape[tape[pos + 2] as usize];
+    tape[res_pos] = tape[p1_pos] * tape[p2_pos];
 
     println!(" {}]", tape[res_pos]);
 }
@@ -63,72 +96,37 @@ fn inp(tape: &mut Vec<Int>, pos: usize) {
     stdin()
         .read_line(&mut input)
         .expect("Could not read string from input");
-    input.trim();
+    let input = input
+        .trim()
+        .parse::<Int>()
+        .expect("Did not enter correct integer");
 
-    let input = input.parse::<Int>().expect("Did not enter correct integer");
+    let input_pos = resolve_pos(pos + 1, &vec![Mode::Address], &tape);
+    tape[input_pos] = input;
+    println!("Storing input at {}", input_pos);
+}
+
+fn out(tape: &Vec<Int>, pos: usize, modes: Vec<Mode>) {
+    let out_pos = resolve_pos(pos + 1, &modes, &tape);
+
+    println!("Output: {}", tape[out_pos]);
 }
 
 // Run the instructions on the tape provided. Returns the tape after the calculation is done
 pub fn run(mut tape: Vec<Int>) -> Vec<Int> {
     let mut pos: usize = 0;
-    while tape[pos] != CODE_EXIT {
-        match tape[pos] {
-            CODE_ADD => add(&mut tape, pos),
-            CODE_MUL => mul(&mut tape, pos),
-            other => panic!("Found invalid opcode [{}]", other),
+    loop {
+        let (op, modes) = split_op_code(tape[pos]);
+
+        match op {
+            Operation::Add => add(&mut tape, pos, modes),
+            Operation::Mul => mul(&mut tape, pos, modes),
+            Operation::Inp => inp(&mut tape, pos),
+            Operation::Out => out(&tape, pos, modes),
+            Operation::Exit => break tape,
         }
 
         // Go to the next instruction
-        pos += 4;
-    }
-
-    tape
-}
-
-fn main() {
-    let input = fs::read_to_string("input").expect("Could not read input file");
-
-    // Convert the input to a `Vec`.
-    let mut tape: Vec<Int> = input
-        .split(',')
-        .map(|x| {
-            x.trim()
-                .parse::<Int>()
-                .expect("Unable to parse input string. Not all numbers")
-        })
-        .collect();
-
-    // Set to the 1202 error state
-    tape[1] = 12;
-    tape[2] = 2;
-
-    println!("Running part one");
-
-    let solution = run(tape.clone())[0];
-    println!("The output is {}", solution);
-
-    println!("Running part two");
-
-    // Run through all number combinations. Since add and mul are commutative, j can start at i (i
-    // <= j)
-    for i in 0..100 {
-        for j in i..100 {
-            println!("Running tape with inputs ({}, {})", i, j);
-
-            tape[1] = i;
-            tape[2] = j;
-
-            let result = run(tape.clone())[0];
-            println!("Result is: {}", result);
-            if result == PART_TWO_EXPECTED {
-                println!("--------------------------------------------------------------");
-                println!(
-                    "The smallest solution for part two was found: {}",
-                    i * 100 + j
-                );
-                println!("--------------------------------------------------------------");
-                break;
-            }
-        }
+        pos += op.num_params() + 1;
     }
 }
--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate enum_primitive_derive;
+
 mod int_machine;
 
 use int_machine::{self as machine, Int};
@@ -7,7 +10,7 @@ fn main() {
     let input = fs::read_to_string("input").expect("Could not read input file");
 
     // Convert the input to a `Vec`.
-    let mut tape: Vec<Int> = input
+    let tape: Vec<Int> = input
         .split(',')
         .map(|x| {
             x.trim()
